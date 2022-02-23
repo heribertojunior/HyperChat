26cc39214411457f094d6f02895f01d407f48de4
heribertojunior <hsc.jr94@gmail.com>
2022-02-23 15:10:42 -0400

    authentication and tabs

--- lib/telas/abaconversa.dart
+++ lib/telas/abaconversa.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hyperchatt/model/conversa.dart';
 
 class AbaConversa extends StatefulWidget {
   const AbaConversa({Key? key}) : super(key: key);
@@ -8,10 +9,18 @@ class AbaConversa extends StatefulWidget {
 }
 
 class _AbaConversaState extends State<AbaConversa> {
+
+  List<Conversa> listaConversas = [
+    Conversa("Jose Renato","Olá tudo bem ?","images/pessoas/pessoa1.jpg"),
+    Conversa("Jose Renato","Olá tudo bem ?","images/pessoas/pessoa1.jpg"),
+  ];
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Convesas"),
+    return ListView.builder(
+      itemCount: listaConversas.length,
+      itemBuilder: (BuildContext context, int index) {
+        return Card()
+      },
     );
   }
 }
